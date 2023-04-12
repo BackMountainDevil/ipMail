0e2df531ceead0e706ce65046749842fc59c48ed
kearney <191615342@qq.com>
2023-04-12 08:40:49 +0800

    update:add third party smtp, test passed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import re
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
 
 def get_ip(netcard="wlan0"):
@@ -11,6 +14,32 @@ def get_ip(netcard="wlan0"):
     else:
         now_ip = ""
     return now_ip
+
+
+def send_email(
+    from_addr, password, to_addr, subject, content, smtp_server, smtp_port=465
+):
+    # send email via smtp
+    ret = True
+    try:
+        msg = MIMEText(content, "plain", "utf-8")
+        msg["From"] = formataddr(["Sender", from_addr])
+        msg["To"] = formataddr(["Receiver", to_addr])
+        msg["Subject"] = subject
+
+        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+        server.login(from_addr, password)
+        server.sendmail(
+            from_addr,
+            [
+                to_addr,
+            ],
+            msg.as_string(),
+        )
+        server.quit()
+    except Exception:
+        ret = False
+    return ret
 
 
 # python main func
