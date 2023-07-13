2c4653bd3303ee01ce7ae69aa20eba21b73236d5
mifen <191615342@qq.com>
2023-07-13 10:10:34 +0800

    feat: ipv6

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 from email.utils import formataddr
 
 
-def get_ip(netcard="enp2s0"):
+def get_ip(netcard="wlan0"):
     """use command ifconfig to get internet ipv4 of netcard wlan0
 
     Args:
@@ -30,6 +30,29 @@ def get_ip(netcard="enp2s0"):
     else:
         now_ip = ""
     return now_ip
+
+
+def get_ipv6(netcard="wlan0"):
+    """use command ifconfig to get internet ipv6 address of netcard
+
+    Args:
+        netcard (str, optional): 网卡名称，默认是无线网卡 "wlan0"，网线一般是 "lo".
+        如果网卡不存在，将会返回ifconfig的错误信息：
+            "error fetching interface information: Device not found"
+
+    Returns:
+        string: ipv6 address if netcard exist.
+    """
+    cmd = ["ifconfig", netcard]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception(f"Command {cmd} failed with error: {err.decode()}")
+    output = out.decode()
+    start_str = "inet6"
+    start = output.find(start_str)
+    end = output.find("prefixlen")
+    return output[start + len(start_str) : end].strip()  # 切片去除空格
 
 
 def getConfig(section, option, configFile="config.ini"):
@@ -94,19 +117,22 @@ def send_email(
 def main():
     # user function get_ip to get ip every hour, if ip change, then send an email
     ip_now = get_ip()  # 获取当前ip
-    global ip
-    if not ip_now == ip:
-        print("ip update to:", ip_now)
-        send_email(content=ip_now)
+    ipv6_now = get_ipv6()
+    global ip, ipv6
+    if not ip_now == ip or not ipv6_now == ipv6:
+        print("ip update to:", ip_now, ipv6_now)
+        send_email(content=ip_now + "\n" + ipv6_now)
         ip = ip_now
+        ipv6 = ipv6_now
     else:
-        print("ip not change:", ip_now)
+        print("ip not change:", ip_now, ipv6_now)
     threading.Timer(3600, main).start()  # 3600s 后调用该函数
 
 
 # python main func
 if __name__ == "__main__":
     ip = get_ip()
-    print("ip:", ip)
-    send_email(content=ip)
+    ipv6 = get_ipv6()
+    print("ipv4:", ip, "\tipv6:", ipv6)
+    send_email(content=ip + "\n" + ipv6)
     main()
